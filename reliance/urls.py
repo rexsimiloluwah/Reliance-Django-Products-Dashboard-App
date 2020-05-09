2dc1f5166bb7c995af23a02eca971455e2e405fa
rexsimiloluwah <38814629+rexsimiloluwah@users.noreply.github.com>
2020-05-09 23:01:23 +0100

    Add files via upload

---
+++ reliance/urls.py
@@ -0,0 +1,35 @@
+"""reliance URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+#Import the settings and static modules fro django.conf 
+from django.conf import settings 
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),
+]
+
+#Configuring the urlpatterns for the media url directory 
+
+urlpatterns += static(settings.MEDIA_URL,
+                        document_root = settings.MEDIA_ROOT)
+
+
+
+
